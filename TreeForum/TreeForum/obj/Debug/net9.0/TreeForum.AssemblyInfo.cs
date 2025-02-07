0c9747ec02e5be832d2677a70eaf31183e015d98
romelt777 <romel.tan777@gmail.com>
2025-02-07 19:30:01 -0400

    able to show to image for discussion index.

--- TreeForum/TreeForum/obj/Debug/net9.0/TreeForum.AssemblyInfo.cs
+++ TreeForum/TreeForum/obj/Debug/net9.0/TreeForum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b959468bda6b186e282479684c4e4d8aafd16046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc56e040d07b79f8411f43b9481789d7140b6fd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
