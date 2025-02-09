3a0b92010a07fcc510d3d4070fe189a93c3ee332
romelt777 <romel.tan777@gmail.com>
2025-02-08 22:50:08 -0400

    changed colors

--- TreeForum/TreeForum/obj/Debug/net9.0/TreeForum.AssemblyInfo.cs
+++ TreeForum/TreeForum/obj/Debug/net9.0/TreeForum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+968099c9135c13fe1a34ab7c019e112568b391e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6716e2a964f6b46d35cd5b42a2902b7d1ba23a2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
