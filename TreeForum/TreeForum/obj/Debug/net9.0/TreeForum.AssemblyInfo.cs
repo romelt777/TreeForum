08e2aca3ed99f139d77478357fff1f1e9f308acd
romelt777 <romel.tan777@gmail.com>
2025-03-08 17:32:56 -0400

    changed styling of nav bar

--- TreeForum/TreeForum/obj/Debug/net9.0/TreeForum.AssemblyInfo.cs
+++ TreeForum/TreeForum/obj/Debug/net9.0/TreeForum.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+980421205462c903b45ca3db0dfbb48e84f87d52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f65b0e053e27e86142966b166a63c4788d6b5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
