005776f9503de26e9550278ea0dc727ce3cdee80
romelt777 <romel.tan777@gmail.com>
2025-03-08 20:11:25 -0400

    changed styling , pre profile image add

--- TreeForum/TreeForum/obj/Debug/net9.0/TreeForum.AssemblyInfo.cs
+++ TreeForum/TreeForum/obj/Debug/net9.0/TreeForum.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe610e471666a0ccfef1ab8f263aae15d11fd561")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dbcaf095101d4cd1e9ca61a11655b800d30b144")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
