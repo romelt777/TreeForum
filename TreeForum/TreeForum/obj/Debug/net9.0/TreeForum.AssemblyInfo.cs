b9cf790349bd4038b0c5c7cf4b71d7e130fff97c
romelt777 <romel.tan777@gmail.com>
2025-03-21 17:34:08 -0300

    adding screenshots for readme

--- TreeForum/TreeForum/obj/Debug/net9.0/TreeForum.AssemblyInfo.cs
+++ TreeForum/TreeForum/obj/Debug/net9.0/TreeForum.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee90f9917587d5ac12162dfa84dfec2b27e07e02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+623ba33925e565d6aefce6154f7af89c1bbd561e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreeForum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
